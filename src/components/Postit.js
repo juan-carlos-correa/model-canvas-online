1bb661fed31e27f722a0011c6228b04d45f5ab5b
juan-carlos-correa <juan.correa.herrera@gmail.com>
2019-05-21 00:22:26 -0500

    add UI Postit component

---
+++ src/components/Postit.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Postit = ({ text }) => (
+  <div className="postit">{text}</div>
+)
+
+export default Postit
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from './Title'
+import Postit from './Postit'
 
-const Section = ({ name }) => (
-  <section>
-    <Title>{name}</Title>
-  </section>
-)
+const Section = ({ name }) => {
+  const [postit, setPostit] = useState([{ text: 'My Post-it example' }])
+
+  return (
+    <section>
+      <Title>{name}</Title>
+
+      {
+        postit.map(({ text }, index) => (
+          <Postit key={`${index}-${text}`} text={text} />
+        ))
+      }
+    </section>
+  )
+}
 
 export default Section
