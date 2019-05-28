85ae5a37630604dcf9d8f7acba53d9c6b932cf2f
juan-carlos-correa <juan.correa.herrera@gmail.com>
2019-05-27 22:47:36 -0500

    Updates

---
+++ precache-manifest.c314234c7facd85651ca542e520c111f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af391c60660e2949b4240c662ba36e1f",
+    "url": "/model-canvas-online/index.html"
+  },
+  {
+    "revision": "752268c6c715ca2543e3",
+    "url": "/model-canvas-online/static/css/main.a6ab4c26.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/model-canvas-online/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "752268c6c715ca2543e3",
+    "url": "/model-canvas-online/static/js/main.f3930e79.chunk.js"
+  },
+  {
+    "revision": "1cc8708b47a49610143c",
+    "url": "/model-canvas-online/static/js/runtime~main.c860e317.js"
+  }
+]);
